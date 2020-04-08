165f09dcf938082288045eedb60f7f2105edebff
akiohansen <akiohansen@users.noreply.github.com>
2020-04-08 20:39:27 +0200

    Create arduino_secrets.h
    User defined settings file.

---
+++ arduino_secrets.h
@@ -0,0 +1,15 @@
+// WiFi parameters
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+// GPRS/3G GSM parameters
+#define SECRET_PINNUMBER "YOURPIN"
+
+// Celluar network parameters
+#define gsmapn ""
+#define gsmlogin ""
+#define gsmpasswd ""
+
+// FTP settings
+#define ftpserver "domainname"
+#define ftpuser "USER user"
+#define ftppasswd "PASS password"
